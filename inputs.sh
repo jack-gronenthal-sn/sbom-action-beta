84ff15b5d46d00a3860dbe82ed9a409f4c91385b
jack.gronenthal <jack.gronenthal@servicenow.com>
2024-04-26 10:58:04 -0700

    Adding messages

--- inputs.sh
+++ inputs.sh
@@ -5,10 +5,14 @@ ERR_MSG__MISSING_SBOM_DIRECTORY="When using the repository SBOM selection strate
 
 echo "::group::SBOM Integration Inputs"
 
+# Validate
 if [ "$SBOM_SOURCE_STRATEGY" = "$REPOSITORY_SOURCE_STRATEGY" ]; then
   if [ -z "$SBOM_SOURCE_DIRECTORY" ]; then
-    echo "::error title={Missing SBOM Source Directory}::{$ERR_MSG__MISSING_SBOM_DIRECTORY}"
+    echo "::error title=Missing SBOM Source Directory::$ERR_MSG__MISSING_SBOM_DIRECTORY"
+    exit 2
   fi
+    echo "::notice title=SBOM Source::Integration will select SBOM file from $SBOM_SOURCE_DIRECTORY"
+"
     echo "Hurray! $SBOM_SOURCE_DIRECTORY, $SBOM_SOURCE_STRATEGY"
 fi
 
