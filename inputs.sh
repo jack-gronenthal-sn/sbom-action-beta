9b3ebf5c165a466fb32115d918241717500aa905
jack.gronenthal <jack.gronenthal@servicenow.com>
2024-04-26 10:47:01 -0700

    Input validation for selection method

--- inputs.sh
+++ inputs.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -l
 
+REPOSITORY_SOURCE_STRATEGY="repository"
+ERR_MSG__MISSING_SBOM_DIRECTORY="When using the repository SBOM selection strategy, an SBOM source directory must be provided."
+
 echo "::group::SBOM Integration Inputs"
-echo "ServiceNow Instance URL: $SERVICE_NOW_INSTANCE_URL"
+
+if [ "$SBOM_SOURCE_STRATEGY" = "$REPOSITORY_SOURCE_STRATEGY" ]; then
+  if [ -z "$SBOM_SOURCE_DIRECTORY" ]; then
+    echo "::error title={Missing SBOM Source Directory}::{$ERR_MSG__MISSING_SBOM_DIRECTORY}"
+  fi
+    echo "Hurray! $SBOM_SOURCE_DIRECTORY, $SBOM_SOURCE_STRATEGY"
+fi
+
 echo "::endgroup::"
